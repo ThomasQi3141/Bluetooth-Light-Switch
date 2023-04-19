0a3fb2e89debc89835d1ef68c9db5cf7fea0d7e0
ThomasQi3141 <131242218+ThomasQi3141@users.noreply.github.com>
2023-04-19 01:45:38 -0700

    Add files via upload

---
+++ Bluetooth_Servo_Control.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+
+Servo Servo1;
+Servo Servo2;
+
+char Incoming_value = 0;
+
+
+void setup() {
+  Serial.begin(9600);   
+  Servo1.attach(13);
+  Servo2.attach(12);
+  Servo1.write(60);
+  Servo2.write(60);
+}
+
+void loop() {
+  if(Serial.available() > 0)  
+  {
+    Incoming_value = Serial.read();      
+    Serial.print(Incoming_value);        
+    Serial.print("\n");        
+    if(Incoming_value == '1') {
+      Servo1.write(0);
+      delay(200);
+      Servo1.write(60);
+    } else if(Incoming_value == '0') {
+      Servo2.write(0);
+      delay(200);
+      Servo2.write(60);
+    }
+  }
+}
